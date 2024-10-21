78e758903dbd3263bcaef94a862a8d791d4e2e64
GabrielBPID <gabrielbrunopereira.id@gmail.com>
2024-10-21 08:03:19 +0700

    adding comment controller

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function send(Request $request){
+        //vaidate request
+        $validatedData = $request=>validate([
+            'name' => ['required', 'string', 'max:50'],
+            'comment' => ['required', 'string', 'max:500'],
+        ]);
+
+        //Sanitize
+        $validatedData['name'] = preq_replace('/([^a-zA-Z0-9\- ])/', '', $validatedData ['name']);
+        $validatedData['comment'] = preg_replace('/([a-zA-Z0-9\- ])/', '', $validatedData['comment']);
+
+        // Save
+        $comment = new Comment();
+        $comment->name = $validatedData['name'];
+        $comment-> =$validatedData['comment'];
+        $comment->save();
+
+        return back();
+    }
+}
