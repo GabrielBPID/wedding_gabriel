22270f6cc951a029f4605b8053225555857a37f5
GabrielBPID <gabrielbrunopereira.id@gmail.com>
2024-10-23 07:57:54 +0700

    modifiy web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
+Route::post('/send-comment', [CommentController::class, 'send']);
